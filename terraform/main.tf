72e817fe1b21b29d41ab42c1950b1a86c8dea58f
Iago Ferreira <iagomauricio@gmail.com>
2022-06-11 21:48:22 -0300

    create api

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
@@ -5,29 +6,56 @@ provider "aws" {
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
-data "aws_ami" "ami_squid" {
+data "aws_ami" "ami_via_cep" {
   most_recent = true
-
-  owners = ["self"]
-  tags = {
-    Name   = "packer-api-cep"
+  owners      = ["self"]
+  filter {
+    name   = "name"
+    values = ["packer-api-cep"]
   }
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ami_squid.id
-  instance_type = "t2.micro"
-
+  ami                         = data.aws_ami.ami_via_cep.id
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.allow_http.id]
   tags = {
-    Name = "Squid"
+    Name = "api_via_cep"
   }
+  iam_instance_profile = "ec2-role"
+  key_name             = "curso"
 }
 
-output "id_ami" {
-  value = data.aws_ami.ami_squid.id
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
